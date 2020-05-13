adc5c70b1fb7331acb1ee39e413084220e370c37
Christopher Sommers <cdsommers@gmail.com>
2020-05-13 14:22:13 -0400

    Add files via upload

---
+++ src/main/java/com/udelblue/services/QRService.java
@@ -0,0 +1,41 @@
+package com.udelblue.services;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+@Service
+public class QRService {
+
+	public BitMatrix generateQRCodeImage(String text, int width, int height) throws WriterException {
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+		return bitMatrix;
+	}
+
+}
